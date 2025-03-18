704fe45237f2d02fd5c30dfb75bca698d1f837d7
stas96111 <stas96111@gmail.com>
2025-03-18 04:48:30 +0100

    Parser fix

--- olx_parser/__init__.py
+++ olx_parser/__init__.py
@@ -1,3 +1,17 @@
 from arguments import args_parse
 from loader import get_page
 from parser import parse_page
+
+# Parse arguments
+args = args_parse()
+
+site_parsed_data = []
+
+for i in range(1, args["pages"] + 1):
+    # Request page from site
+    page_content = get_page(args["url"], i)
+    
+    # Parse page and get taitle, price, date, location and url
+    site_parsed_data += parse_page(page_content)
+    
+print(site_parsed_data)
--- olx_parser/parser.py
+++ olx_parser/parser.py
@@ -1,22 +1,24 @@
 from bs4 import BeautifulSoup
 
+base_url = "https://www.olx.ua"
+
 def parse_page(page_content):
     soup = BeautifulSoup(page_content, 'html.parser')
     
-    items_title_div = soup.find('div', {'data-cy': 'ad-card-title'})
-    items_price_p = soup.find('p', {'data-testid': 'ad-price'})
-    items_data_time_p = soup.find('p', {'data-testid': 'location-date'})
+    items_title_div = soup.find_all('div', {'data-cy': 'ad-card-title'})
     
     output_data = []
     
     for item in items_title_div:
-        title = item.text.strip()
-        price = items_price_p.text
+        price = item.find('p', {'data-testid': 'ad-price'}).text
+        data_location = item.find_next('p', {'data-testid': 'location-date'})
         
         # Split location and date
-        location, date = items_data_time_p.text.split(' - ')
+        location, date = data_location.text.split(' - ')
         
-        url = item.find('a')['href']
+        link = item.find('a')
+        url = base_url + link['href']
+        title = link.text
         
         output_data.append((title, price, date, location, url))
         
